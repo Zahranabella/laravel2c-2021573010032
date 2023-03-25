86c7e6bcf1ef1938f8311f98fe7c99732fdbabce
Zahranabella <zhranabella@gmail.com>
2023-03-25 14:27:52 +0700

    header and footer

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/mahasiswa">Data Mahasiswa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/dosen">Data Dosen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/gallery">Gallery</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
